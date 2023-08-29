fbd7ac10d01f7909bb00b196b56368e98487b664
anastasia7makar <anastasia7makar@gmail.com>
2023-08-29 17:57:59 +0300

    Added loader

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,17 @@
 import { fetchContacts } from 'redux/operations';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import ContactForm from './ContactForm/ContactForm';
 import ContactList from './ContactList/ContactList';
 import Filter from './Filter/Filter';
 import Section from './Section/Section';
 import { useEffect } from 'react';
+import { selectError, selectIsLoading } from 'redux/selectors';
+import { ThreeDots } from 'react-loader-spinner';
 
 export const App = () => {
   const dispatch = useDispatch();
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
 
   useEffect(() => {
     dispatch(fetchContacts());
@@ -20,6 +24,18 @@ export const App = () => {
       </Section>
       <Section title="Contacts">
         <Filter />
+        {isLoading && !error && (
+          <ThreeDots
+            height="80"
+            width="80"
+            radius="9"
+            color="#1f86e0"
+            ariaLabel="three-dots-loading"
+            wrapperStyle={{}}
+            wrapperClassName=""
+            visible={true}
+          />
+        )}
         <ContactList />
       </Section>
     </div>
